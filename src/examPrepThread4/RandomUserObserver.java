9b97ebe1bd64c98417af41a79959fc780ed7fd9b
jonassimonsen <jonassimonsen@10.50.137.12>
2015-08-28 14:02:39 +0200

    First Commit

---
+++ src/examPrepThread4/RandomUserObserver.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examPrepThread4;
+
+import java.util.Observable;
+
+/**
+ *
+ * @author jonassimonsen
+ */
+public interface RandomUserObserver {
+    void update (Observable o, Object arg);
+    
+}
